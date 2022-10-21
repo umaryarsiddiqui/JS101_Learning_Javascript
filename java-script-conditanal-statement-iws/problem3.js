a6b37a53dd8ce7f9e557a6dc85edb4859ff74aea
umaryarsiddiqui <umaryarsiddiqui@gmail.com>
2022-10-21 07:22:49 +0530

    is me umar

---
+++ java-script-conditanal-statement-iws/problem3.js
@@ -0,0 +1,9 @@
+x=3
+y=3
+if(x>y){
+  console.log("x is greater")
+}else if(x<y){
+  console.log("y is greater")
+}else if(x==y){
+  console.log("both equal");
+}
---
+++ java-script-conditanal-statement-iws/problem4.js
@@ -0,0 +1,7 @@
+let stored_username ="umarsiddiqui";
+let stored_password ="umar1212";
+let input_username ="umarsiddiqui";
+let input_password ="umar1212";
+stored_username==input_username && stored_password==input_password ?
+  console.log("user can login")
+: console.log(" not");
